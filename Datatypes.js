09b48ba1d05162934f165105fba9c32429db0298
Harkonnen <gunjotikaraditya@gmail.com>
2026-09-12 07:05:34 +0530

    1.0

---
+++ Datatypes.js
@@ -0,0 +1,7 @@
+let str = "hello";       // string
+let num = 42;             // number
+let bool = false;         // boolean
+let arr = [1, 2, 3];       // array
+let obj = { key: "value" }; // object
+let nothing = null;        // intentional empty value
+let notDefined;             // undefined
