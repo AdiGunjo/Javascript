0d5d96b140434fa902af4e00916d7be7ac9b5bbb
Harkonnen <gunjotikaraditya@gmail.com>
2026-09-12 07:04:27 +0530

    1.0

---
+++ ArrayBasics.js
@@ -0,0 +1,13 @@
+let fruits = ["apple", "banana", "mango"];
+console.log(fruits[0]);       
+console.log(fruits.length);   
+fruits[1] = "orange";  
+
+let nums = [1, 2, 3];
+nums.push(4);      
+nums.pop();         
+nums.unshift(0);    
+nums.shift();
+
+console.log(nums);
+
